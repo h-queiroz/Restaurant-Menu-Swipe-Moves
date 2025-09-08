904fb07c1bdcc57f2e009c06002e57124a539595
Hector Queiróz <hectorqrs@live.com>
2025-09-08 15:53:18 -0300

    feat: Left and Right Tap
    Made it so that user can control if he wants to see the image the right or the left of current one depending on where he taps on the card

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 let imgLinks = [
     ["foods/food1-1.jpg", "foods/food1-2.jpg", "foods/food1-3.jpg"],
     ["foods/food2-1.jpg", "foods/food2-2.jpg", "foods/food2-3.jpg"],
+    ["foods/food3-1.jpg", "foods/food3-2.jpg", "foods/food3-3.jpg"],
 ];
 
 let mainElement = document.querySelector("main");
 let imgElement = mainElement.children[0];
-imgElement.src = imgLinks[0][0];
+let currentImg = 0;
+imgElement.src = imgLinks[2][currentImg];
 
-let currentImg = 1;
-mainElement.addEventListener("click", () => {
-    if(currentImg > 2)
-        currentImg = 0;
 
-    imgElement.src = imgLinks[0][currentImg++];
+mainElement.addEventListener("click", (event) => {
+
+    // If clicks to on the left half of the card
+    if(event.clientX < mainElement.offsetLeft){
+        if(--currentImg < 0)
+            currentImg = 2;
+    } else {
+        if(++currentImg > 2)
+            currentImg = 0;
+    }
+
+    imgElement.src = imgLinks[2][currentImg];
 });
